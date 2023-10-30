354083a66bb3d0e4caa53d1900180f29c6dbc1dd
Sami EL KATEB <67482346+SamiElkateb@users.noreply.github.com>
2023-10-30 23:27:04 +0100

    add: remote bash config

--- private_dot_config/tmux/executable_tmux-sessionizer.sh
+++ private_dot_config/tmux/executable_tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find "$HOME/repos/" "$HOME/workspace/" "$HOME/.config" "$HOME/.local/share/" -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find "$HOME/repos/" "$HOME/workspace/" "$HOME/.config" "$HOME/godot" "$HOME/work" "$HOME/.local/share/" "$HOME/polytech/" -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
